a54629f4cb5530fe5c4374818ed9f92cdbdb3133
BYF-AI-creat <3303073212@qq.com>
2026-06-07 14:39:48 +0800

    finish second version:add INPUT function and update

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,2 +1,15 @@
-name = input("Please enter your name: ") 
-print("Hello, " + name) 
+#include <stdio.h>
+
+int main() {
+    char input[100]; // 准备一个小盒子用来装你输入的文字
+    printf("Hello World!\n");
+    printf("请输入一些内容: ");
+
+    // 使用 scanf 获取键盘输入的内容
+    scanf("%s", input);
+
+    // 使用 printf 把你输入的内容显示出来
+    printf("你输入的内容是: %s\n", input);
+
+    return 0;
+}
